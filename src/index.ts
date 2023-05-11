28f0ebc449d79536c729add57f3bda91921aa457
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-11 12:40:03 +0000

    11 May 2023, 18:10

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import diaryRouter from './routes/diaries';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 const PORT = 3001;
 
